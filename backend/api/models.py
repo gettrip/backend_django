c62687fa8887654dcffe48177324badd3248f2c4
Valexandrin <alex-vadim@mail.ru>
2022-05-16 16:41:46 +0300

    all models created

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,25 +1,48 @@
 from django.db import models
 
 
-class City(models.Model):    
-    name = models.CharField(max_length=100, blank=False, unique=True)
-    image = models.CharField(max_length=100, blank=False)
+class User(models.Model):        
+    uid = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=100, unique=True)
+
+
+class City(models.Model):   
+    uid = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100, unique=True)
+    image = models.CharField(max_length=100)
     
 
-class Place(models.Model):    
-    name = models.CharField(max_length=100, blank=False, unique=True)
-    city_id = models.ForeignKey(City, blank=False, null=True, on_delete=models.SET_NULL)
-    image = models.CharField(max_length=100, blank=False)
+class Place(models.Model):
+    uid = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    city_id = models.ForeignKey(City, on_delete=models.CASCADE, db_column='city_id')
+    image = models.CharField(max_length=100)
+    description = models.TextField()
+    duration = models.PositiveIntegerField() 
+
+    class Meta:
+        constraints = [models.UniqueConstraint(fields=['name', 'city_id'], name='unique_city_place')]
+
+
+class Route(models.Model):
+    uid = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    city_id = models.ForeignKey(City, on_delete=models.CASCADE, db_column='city_id')
+    image = models.CharField(max_length=100)
     description = models.TextField()
     duration = models.PositiveIntegerField()
 
+    class Meta:
+        constraints = [models.UniqueConstraint(fields=['name', 'city_id'], name='unique_city_route')]
 
-class Route(models.Model):    
-    name = models.CharField(max_length=200, blank=False, unique=True)
-    city_id = models.ForeignKey(City, blank=False, null=True, on_delete=models.SET_NULL)
-    image = models.CharField(max_length=100, blank=False)
-    description = models.TextField()
-    duration = models.PositiveIntegerField()
-    places = models.ManyToManyField(Place)
 
-    models.UniqueConstraint(fields=[name, city_id], name='unique_city_route')
+class RoutePoint(models.Model):
+    position = models.PositiveIntegerField(blank=False)
+    place_id = models.ForeignKey(Place, on_delete=models.CASCADE)
+    route_id = models.ForeignKey(Route, on_delete=models.CASCADE)
+    distance = models.PositiveIntegerField(blank=False)    
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['place_id', 'route_id'], name='unique_routepoint'),
+            models.UniqueConstraint(fields=['position', 'route_id'], name='unique_position')
+        ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,11 @@
-from .models import City, Place, Route
-from .serializers import CitySerializer, PlaceSerializer, RouteSerializer
+from .models import City, Place, Route, User, RoutePoint
+from .serializers import CitySerializer, PlaceSerializer, RouteSerializer, UserSerializer, RoutePointSerializer
 from rest_framework import viewsets
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 class CityViewSet(viewsets.ModelViewSet):
     queryset = City.objects.all()
@@ -13,3 +18,16 @@ class PlaceViewSet(viewsets.ModelViewSet):
 class RouteViewSet(viewsets.ModelViewSet):
     queryset = Route.objects.all()
     serializer_class = RouteSerializer
+
+class CityRoutesViewSet(RouteViewSet):    
+    def get_queryset(self):        
+        city_id = self.kwargs['city_id']               
+        return Route.objects.filter(city_id=city_id)        
+
+class RoutePointViewSet(viewsets.ModelViewSet):
+    queryset = RoutePoint.objects.all()    
+    serializer_class = RoutePointSerializer
+
+    def get_queryset(self):
+        route_id = self.kwargs['route_id']        
+        return RoutePoint.objects.filter(route_id=route_id)

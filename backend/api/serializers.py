7773b331742cea1291af503bf066e919a2e1ee21
Valexandrin <alex-vadim@mail.ru>
2022-06-06 09:03:52 +0300

    corrections by linters

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,31 +1,33 @@
 from rest_framework import serializers
+
 from .models import City, Place, Route, RoutePoint, User
 
+
 class UserSerializer(serializers.ModelSerializer):
-    class Meta:
+    class Meta(object):
         model = User
         fields = ('uid', 'username')
 
 
 class CitySerializer(serializers.ModelSerializer):
-    class Meta:
+    class Meta(object):
         model = City
         fields = ('uid', 'name', 'image')
 
 
 class PlaceSerializer(serializers.ModelSerializer):
-    class Meta:
+    class Meta(object):
         model = Place
-        fields = ('uid', 'city_id', 'name', 'image', 'description', 'duration')   
+        fields = ('uid', 'city_id', 'name', 'image', 'description', 'duration')
 
 
 class RouteSerializer(serializers.ModelSerializer):
-    class Meta:
+    class Meta(object):
         model = Route
-        fields = ('uid', 'city_id', 'name', 'image', 'description', 'duration')  
+        fields = ('uid', 'city_id', 'name', 'image', 'description', 'duration')
+
 
 class RoutePointSerializer(serializers.ModelSerializer):
-    class Meta:
+    class Meta(object):
         model = RoutePoint
         fields = ('position', 'route_id', 'place_id', 'distance')
-    
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,33 +1,40 @@
-from .models import City, Place, Route, User, RoutePoint
-from .serializers import CitySerializer, PlaceSerializer, RouteSerializer, UserSerializer, RoutePointSerializer
 from rest_framework import viewsets
+
+from .models import City, Place, Route, RoutePoint, User
+from .serializers import (CitySerializer, PlaceSerializer, RoutePointSerializer, RouteSerializer,
+                          UserSerializer)
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+
 class CityViewSet(viewsets.ModelViewSet):
     queryset = City.objects.all()
     serializer_class = CitySerializer
+
 
 class PlaceViewSet(viewsets.ModelViewSet):
     queryset = Place.objects.all()
     serializer_class = PlaceSerializer
 
+
 class RouteViewSet(viewsets.ModelViewSet):
     queryset = Route.objects.all()
     serializer_class = RouteSerializer
 
-class CityRoutesViewSet(RouteViewSet):    
-    def get_queryset(self):        
-        city_id = self.kwargs['city_id']               
-        return Route.objects.filter(city_id=city_id)        
+
+class CityRoutesViewSet(RouteViewSet):
+    def get_queryset(self):
+        city_id = self.kwargs['city_id']
+        return Route.objects.filter(city_id=city_id)
+
 
 class RoutePointViewSet(viewsets.ModelViewSet):
-    queryset = RoutePoint.objects.all()    
+    queryset = RoutePoint.objects.all()
     serializer_class = RoutePointSerializer
 
     def get_queryset(self):
-        route_id = self.kwargs['route_id']        
+        route_id = self.kwargs['route_id']
         return RoutePoint.objects.filter(route_id=route_id)
